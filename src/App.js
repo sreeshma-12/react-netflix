586beca5dd9f49b59fe9f5edc557165a154a9054
sreeshma <sreeshma.t.2019@gmail.com>
2024-03-02 20:33:30 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+import {
+  originals,
+  actions,
+  comedy,
+  horror,
+  romance,
+  trending,
+} from "./urls.js";
+
+const url = [
+  { urls: actions, title: "Action" },
+  { urls: trending, title: "Trending" },
+  { urls: originals, title: "Netflix Originals" },
+  { urls: romance, title: "Romance" },
+  { urls: comedy, title: "Comedy" },
+  { urls: horror, title: "Horror" },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Banner />
+      {url.map((row) => (
+        <RowPost urls={row.urls} title={row.title} />
+      ))}
+      {}
     </div>
   );
 }
